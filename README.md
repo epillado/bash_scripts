cb4319fc429f54861c012629d28bb56ef1b4f053
Lalo <eduardopillado@gmail.com>
2025-04-02 14:45:57 -0600

    Added a variation of process control example.

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Some example Bash scripts
 
 ## List of examples
 - countprocs.sh: This example script counts and groups similar processes.
-- process_control.sh: This is an example of launching some processes in the background, waiting for them to finish, and checking if any failed.
+- proc_control.sh: This is an example of launching some processes in the background, waiting for them to finish, and checking if any failed. It uses a function as the launched process.
+- process_control.sh: This is an example of launching some processes in the background, waiting for them to finish, and checking if any failed. It uses another shell as the launched process.
 
---
+++ proc_control.sh
@@ -0,0 +1,53 @@
+#! /usr/bin/bash
+
+# Author: Eduardo Pillado.
+# https://github.com/epillado
+#
+# This is an example for job control in Bash.
+# We want to launch 5 processes in paralell and count how
+# many of them failed.
+# BASH>= 4 required
+
+# This function will be the launched process.
+example_process() {
+ EXPECTED_RETVAL=$1
+ echo -e "\n$BASHPID Is running."
+ if [ $EXPECTED_RETVAL == 0 ]; then
+  sleep 5
+ fi
+ echo -e "\n$BASHPID finished with status $EXPECTED_RETVAL"
+ exit $EXPECTED_RETVAL
+}
+
+launched(){
+ id=$1 # Receives the process ID as first param.
+ PROCESSES+=($id) # Add the process ID to an array.
+ echo "$id launched."
+}
+
+FAILED=0 #Variable to store count of failed processes.
+declare -a PROCESSES # Array for storing process IDs.
+
+# Let's launch 5 processes in background and make one "fail".
+
+example_process 0 &
+launched $!
+
+example_process 0 &
+launched $!
+
+example_process 1 & # This one will fail
+launched $!
+
+example_process 0 &
+launched $!
+
+example_process 0 &
+launched $!
+
+for p in ${PROCESSES[@]}; do
+	wait $p || (( FAILED++ ))
+done
+
+echo -e "\nFailed processes: $FAILED "
+
